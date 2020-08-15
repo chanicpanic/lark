00d1a87e7901dd41b26ac69db886ec5017a617ca
Chanic Panic <chanicpanic@gmail.com>
2020-08-15 16:31:51 -0700

    edited new forest visitor docstring

--- lark/parsers/earley_forest.py
+++ lark/parsers/earley_forest.py
@@ -369,20 +369,17 @@ class CompleteForestToAmbiguosTreeVisitor(ForestToTreeVisitor):
     An augmented version of ForestToAmbiguousTreeVisitor that is designed to 
     handle ambiguous intermediate nodes as well as ambiguous symbol nodes.
 
-    For ease of implementation, it has not been attempted to construct the
-    AST in the same format as ForestToAmbiguousTreeVisitor during the forest
-    walk. Instead, an intermediate version of the ambiguous AST is 
-    constructed similar to the structure of the SPPF.
+    On the way down:
 
     - When an ambiguous intermediate node is encountered, an '_imambig' node
       is inserted into the tree.
     - Each possible derivation of an ambiguous intermediate node is represented 
       by an '_inter' node added as a child of the corresponding '_imambig' node.
 
-    After the intermediate ambiguous AST is constructed, the intermediate
-    ambiguities are propagated up to the nearest rule and replaced with
-    '_ambig' nodes using the RemoveIntermediateAmbiguities Transformer,
-    returning a tree in the expected format.
+    On the way up, these nodes are propogated up the tree and collapsed
+    into a single '_ambig' node for the nearest symbol node ancestor.
+    This is achieved by the AmbiguousIntermediateExpander contained in
+    the callbacks.
     """
 
     def _collapse_ambig(self, root, children):
